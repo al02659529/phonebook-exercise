133a72b1f614216d0246b15cfa50dbeaa0c0138b
al02659529 <44920146+al02659529@users.noreply.github.com>
2020-07-05 13:54:36 -0600

    added front-end

---
+++ build/precache-manifest.1395e9238499eea7685358605b33d705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62557fb0fee6c5cc5a6b70b89617d374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc319d77f3e58067909",
+    "url": "/static/css/main.946d21f9.chunk.css"
+  },
+  {
+    "revision": "22dc9e107032abd95747",
+    "url": "/static/js/2.37ac9c20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37ac9c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc319d77f3e58067909",
+    "url": "/static/js/main.32441712.chunk.js"
+  },
+  {
+    "revision": "9fc6b00e84a58d0d3fa7",
+    "url": "/static/js/runtime-main.d0e0eec5.js"
+  },
+  {
+    "revision": "9622dd4dde11360b993b8b4097e54f53",
+    "url": "/static/media/symbol-defs.9622dd4d.svg"
+  }
+]);
